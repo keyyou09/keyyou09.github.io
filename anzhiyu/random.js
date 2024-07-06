80b8f8cb258537db59b4da60f76ad95e2d77201c
unknown <keyyou97@outlook.com>
2024-07-07 00:49:51 +0800

    Site updated: 2024-07-07 00:49:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/04/11/","2024/07/04/1/11/","2024/07/04/抖音/","2024/07/03/hello-world/"];function toRandomPost(){
+var posts=["2024/07/07/2/","2024/07/04/11/","2024/07/04/1/11/","2024/07/04/douyin/","2024/07/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
